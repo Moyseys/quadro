40ea748e2274087e79a743445df5f594d13865b7
Moyseys <moyseys.veroni@gmail.com>
2024-09-14 23:29:31 -0300

    Update DataBase

--- database/quadro.sql
+++ database/quadro.sql
@@ -1,190 +1,185 @@
-CREATE DATABASE  IF NOT EXISTS `quadro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `quadro`;
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
---
--- Host: localhost    Database: quadro
--- ------------------------------------------------------
--- Server version	8.2.0
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Table structure for table `banlance`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema quadro
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `banlance`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `banlance` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `value` decimal(15,4) DEFAULT NULL,
-  `team_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_team` (`team_id`),
-  CONSTRAINT `banlance_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Schema quadro
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `quadro` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `quadro` ;
 
---
--- Dumping data for table `banlance`
---
+-- -----------------------------------------------------
+-- Table `quadro`.`team`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`team` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `description` VARCHAR(500) NOT NULL,
+  `value` DECIMAL(8,4) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-LOCK TABLES `banlance` WRITE;
-/*!40000 ALTER TABLE `banlance` DISABLE KEYS */;
-/*!40000 ALTER TABLE `banlance` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `fixed_income`
---
+-- -----------------------------------------------------
+-- Table `quadro`.`tag`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`tag` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `theme` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `fixed_income`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `fixed_income` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `value` decimal(15,4) DEFAULT NULL,
-  `period` int DEFAULT NULL,
-  `user_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `fixed_income_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `fixed_income`
---
+-- -----------------------------------------------------
+-- Table `quadro`.`balance`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`balance` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `value` DECIMAL(15,4) NOT NULL,
+  `team_id` INT NOT NULL,
+  `created_at` DATE NOT NULL,
+  `tag_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `tag_id`),
+  UNIQUE INDEX `unique_team` (`team_id` ASC) VISIBLE,
+  CONSTRAINT `balance_ibfk_1`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `quadro`.`team` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `tag_fk_id`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `quadro`.`tag` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-LOCK TABLES `fixed_income` WRITE;
-/*!40000 ALTER TABLE `fixed_income` DISABLE KEYS */;
-/*!40000 ALTER TABLE `fixed_income` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `goal`
---
-
-DROP TABLE IF EXISTS `goal`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `goal` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) DEFAULT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `value` decimal(10,4) DEFAULT NULL,
-  `team_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `team_id` (`team_id`),
-  CONSTRAINT `goal_ibfk_1` FOREIGN KEY (`id`) REFERENCES `team` (`id`),
-  CONSTRAINT `team_id` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `goal`
---
-
-LOCK TABLES `goal` WRITE;
-/*!40000 ALTER TABLE `goal` DISABLE KEYS */;
-/*!40000 ALTER TABLE `goal` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tag`
---
-
-DROP TABLE IF EXISTS `tag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tag` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `theme` varchar(255) DEFAULT NULL,
-  `balance_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `balance_id` (`balance_id`),
-  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`balance_id`) REFERENCES `banlance` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tag`
---
-
-LOCK TABLES `tag` WRITE;
-/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `team`
---
-
-DROP TABLE IF EXISTS `team`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `team` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) DEFAULT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `team`
---
-
-LOCK TABLES `team` WRITE;
-/*!40000 ALTER TABLE `team` DISABLE KEYS */;
-/*!40000 ALTER TABLE `team` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id_user` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table `quadro`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`user` (
+  `id_user` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id_user`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE INDEX `email` (`email` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `user`
---
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- -----------------------------------------------------
+-- Table `quadro`.`fixed_income`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`fixed_income` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `value` DECIMAL(10,4) NOT NULL,
+  `period` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `user_id` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fixed_income_ibfk_1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `quadro`.`user` (`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-27 20:57:28
+-- -----------------------------------------------------
+-- Table `quadro`.`goal`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`goal` (
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `description` VARCHAR(500) NOT NULL,
+  `value` DECIMAL(10,4) NOT NULL,
+  `team_id` INT NOT NULL,
+  INDEX `team_id` (`team_id` ASC) VISIBLE,
+  CONSTRAINT `team_id`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `quadro`.`team` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `quadro`.`invite`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`invite` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `team_id` INT NULL DEFAULT NULL,
+  `sender_user_id` INT NULL DEFAULT NULL,
+  `recipient_user_id` INT NULL DEFAULT NULL,
+  `status` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `team_id` (`team_id` ASC) VISIBLE,
+  INDEX `sender_user_id` (`sender_user_id` ASC) VISIBLE,
+  INDEX `recipient_user_id` (`recipient_user_id` ASC) VISIBLE,
+  CONSTRAINT `invite_ibfk_1`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `quadro`.`team` (`id`),
+  CONSTRAINT `invite_ibfk_2`
+    FOREIGN KEY (`sender_user_id`)
+    REFERENCES `quadro`.`user` (`id_user`),
+  CONSTRAINT `invite_ibfk_3`
+    FOREIGN KEY (`recipient_user_id`)
+    REFERENCES `quadro`.`user` (`id_user`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `quadro`.`team_member`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quadro`.`team_member` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `team_id` INT NULL DEFAULT NULL,
+  `user_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `team_id` (`team_id` ASC) VISIBLE,
+  INDEX `user_id` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `team_member_ibfk_1`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `quadro`.`team` (`id`),
+  CONSTRAINT `team_member_ibfk_2`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `quadro`.`user` (`id_user`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
